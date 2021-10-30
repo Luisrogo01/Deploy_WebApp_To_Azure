0df47be321cd7abfd3392fa3c24eb8a013dc26fa
Luis Rodriguez <luis_01@outlook.com>
2021-10-30 00:20:12 -0500

    start of new repo

---
+++ FlaskWebProject/__init__.py
@@ -0,0 +1,32 @@
+"""
+The flask application package.
+"""
+import logging
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_session import Session
+import sys
+from .header import header
+
+app = Flask(__name__)
+app.config.from_object(Config)
+# TODO: Add any logging levels and handlers with app.logger
+
+app.logger.setLevel(logging.INFO)
+
+streamHandler = logging.StreamHandler(stream=sys.stdout)
+streamHandler.setFormatter(logging.Formatter( '%(levelname)s: - [%(asctime)s] - %(message)s'))
+streamHandler.setLevel(logging.INFO)
+app.logger.addHandler(streamHandler)
+
+app.logger.info('App Startup')
+app.logger.info(header)
+
+Session(app)
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
+
+import FlaskWebProject.views
